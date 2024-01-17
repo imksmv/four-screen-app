b241c451af27ff43a7e9f30743e5c77f47c38931
imksmv <98290303+imksmv@users.noreply.github.com>
2024-01-17 23:07:25 +0200

    Add fonts and images

---
+++ assets/images/index.js
@@ -0,0 +1,11 @@
+import user1 from "./user1.jpeg"
+import user2 from "./user2.jpeg"
+import user3 from "./user3.jpeg"
+import user4 from "./user4.jpeg"
+import user5 from "./user5.jpeg"
+import user6 from "./user6.jpeg"
+import user7 from "./user7.jpeg"
+import user8 from "./user8.jpeg"
+import user9 from "./user9.jpeg"
+
+export { user1, user2, user3, user4, user5, user6, user7, user8, user9 }
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,8 +1,25 @@
-import { View, Text } from "react-native"
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from "react"
+import { Text, View } from "react-native"
 
 const WelcomeScreen = () => {
+  const [fontLoaded, fontError] = useFonts({
+    Inter: require("../fonts/Inter-Regular.ttf"),
+    InterLight: require("../fonts/Inter-Light.ttf"),
+    InterBold: require("../fonts/Inter-Bold.ttf"),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontLoaded || fontError) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontLoaded, fontError])
+
+  if (!fontLoaded) return
+
   return (
-    <View>
+    <View onLayout={onLayoutRootView}>
       <Text>WelcomeScreen</Text>
     </View>
   )
